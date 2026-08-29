425b09ac27c7d6c371fc28967beaf749b07c80f0
jjenkins91 <jasonjenkins4321@gmail.com>
2026-08-29 12:39:08 -0400

    added more to home JSX

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,6 +8,14 @@ function Home() {
         <div className="logo-container">
           <img className="logo" src="penp.png" alt="penpLogo" />
         </div>
+        <div className="title-container">
+            <h1 className="title">PUNTO EQUILIBRIO<br/>
+                NÜA PSICOLOGÍA</h1>
+        </div>
+        <hr className="divider"/>
+        <div className="vision-container">
+        <h2 className="vision">Comprender  -  Sanar  -  Avanzar</h2>
+        </div>
       </div>
     </>
   );
