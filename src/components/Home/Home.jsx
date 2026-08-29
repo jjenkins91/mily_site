09709bae9d113ebaf8d9e386c65f8d1dd172f425
jjenkins91 <jasonjenkins4321@gmail.com>
2026-08-29 11:21:01 -0400

    Added Logo

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,15 @@
-import "./Home.css"
+import "./Home.css";
 import Navbar from "../NavBar/Navbar";
-function Home(){
-    return(
-        <>
-        <div className="header-container">
-        <Navbar/>
+function Home() {
+  return (
+    <>
+      <div className="header-container">
+        <Navbar />
+        <div className="logo-container">
+          <img className="logo" src="penp.png" alt="penpLogo" />
         </div>
-        </>
-    );
+      </div>
+    </>
+  );
 }
 export default Home;
