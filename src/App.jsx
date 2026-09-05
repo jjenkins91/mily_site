03e32675ca8b53189e1b05288e3ac22bc6b8aa80
jjenkins91 <jasonjenkins4321@gmail.com>
2026-09-05 11:42:27 -0400

    Added SyE2.jsx and did text align

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import FT from "./components/FormaTrabajar/FT";
 import Home from "./components/Home/Home";
 import Nosotros from "./components/Nosotros/Nosotros";
 import SyE from "./components/SyE/SyE";
+import SyE2 from "./components/SyE2/SyE2";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Nosotros/>
       <FT/>
       <SyE/>
+      <SyE2/>
     </>
   );
 }
---
+++ src/components/SyE2/SyE2.jsx
@@ -0,0 +1,69 @@
+import "./SyE2.css";
+
+function SyE2() {
+  return (
+    <>
+      <div className="SyE2-container">
+        <h2 className="SyE2-title">Servicios y Especilidades</h2>
+        <div className="servicios-especialidades2">
+          <div className="s-y-e2">
+            <div>
+              <h3 className="header-title">Psicologia para<br />adolescentes</h3>
+              <p className="SyE2-summary2">
+                Espacio de escucha
+                <br />
+                y acompanamiento
+                <br />
+                durante los
+                <br />
+                cambios y desafios
+                <br />
+                propios de esta
+                <br />
+                etapa.
+              </p>
+            </div>
+          </div>
+          <div className="s-y-e2">
+            <div>
+              <h3 className="header-title">Psicologia infantil</h3>
+              <p className="SyE2-summary">
+                Evaluacion y
+                <br />
+                y acompanamiento
+                <br />
+                psicologico
+                <br />
+                dirigido a ninos,
+                <br />
+                segun sus
+                <br />
+                necesidades
+              </p>
+            </div>
+          </div>
+          <div className="s-y-e2">
+            <div>
+              <h3 className="header-title">Neuropsicologia</h3>
+              <p className="SyE2-summary3">
+                Evaluacion y
+                <br />
+                rehabilitacion
+                <br />
+                cognitiva para
+                <br />
+                cuidar tu salud
+                <br />
+                cerebral y calidad 
+                <br />
+                de vida.
+              </p>
+            </div>
+          </div>
+        </div>
+        <h3 className="professional-title">Contamos con profesionals especializados</h3>
+      </div>
+    </>
+  );
+}
+export default SyE2;
