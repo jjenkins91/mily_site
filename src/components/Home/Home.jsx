964817e516582726c03d90c1dfb4a22aad5445c8
jjenkins91 <jasonjenkins4321@gmail.com>
2026-09-07 18:15:25 -0400

    added whatsapp button

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -9,13 +9,27 @@ function Home() {
           <img className="logo" src="penp.png" alt="penpLogo" />
         </div>
         <div className="title-container">
-            <h1 className="title">PUNTO EQUILIBRIO<br/>
-                NÜA PSICOLOGÍA</h1>
+          <h1 className="title">
+            PUNTO EQUILIBRIO
+            <br />
+            NÜA PSICOLOGÍA
+          </h1>
         </div>
-        <hr className="divider"/>
+        <hr className="divider" />
         <div className="vision-container">
-        <h2 className="vision">Comprender  -  Sanar  -  Avanzar</h2>
+          <h2 className="vision">Comprender - Sanar - Avanzar</h2>
         </div>
+        <a
+    className="whatsapp-button"
+    href="https://wa.me/19891363?text=Hola%2C%20me%20gustaría%20agendar%20una%20cita."
+    target="_blank"
+    rel="noopener noreferrer"
+>
+    <img
+        src="/whatsapp_logo.png"
+        alt="WhatsApp"
+    />
+</a>
       </div>
     </>
   );
